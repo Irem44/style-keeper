b90a7c93c963038611070e808fd293f1341984fe
Irem Sude Demir <iremsudedemir1@gmail.com>
2026-03-03 15:29:17 +0300

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import DashboardLayout from "./components/dashboard/DashboardLayout";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Welcome Style Keeper</h1>
+      <DashboardLayout />
     </>
   );
 }
